d1dd799bc137b296cf03bb79b63d1b4923fdacaa
Rex Zeng <rex@rexskz.info>
2023-01-10 14:44:17 +0800

    fix: fix typo for index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import requireIndex from 'requireindex';
 module.exports = {
   rules: requireIndex(path.resolve(__dirname, 'rules')),
   configs: {
-    defailt: {
+    default: {
       plugins: ['try-catch-failsafe'],
       rules: {
         'try-catch-failsafe/json-parse': 'error',
